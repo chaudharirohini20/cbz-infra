6e2ade03762c0d7fede7c4c8e941995e06ee4e49
chaudharirohini20 <chaudharirohini012@gmail.com>
2026-03-18 21:49:45 +0530

    new commit

--- ec2.tf
+++ ec2.tf
@@ -3,7 +3,7 @@ provider "aws" {
      }
  
  resource "aws_instance" "my-instance" {
-    ami = "ami-007dcf089b8078f1a"
+    ami = "ami-05d43d5e94bb6eb95"
     instance_type ="t3.micro"
     vpc_security_group_ids =["sg-0ad7b8699e7476134"]
     key_name ="rohi12"
