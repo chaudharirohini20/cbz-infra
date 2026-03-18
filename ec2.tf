56e0ccf9bbd7975cf4cecf6a379e5ae210eab28a
chaudharirohini20 <chaudharirohini012@gmail.com>
2026-03-18 21:50:47 +0530

    fix

--- ec2.tf
+++ ec2.tf
@@ -4,7 +4,7 @@ provider "aws" {
  
  resource "aws_instance" "my-instance" {
     ami = "ami-05d43d5e94bb6eb95"
-    instance_type ="t3.micro"
+    instance_type ="t3.small"
     vpc_security_group_ids =["sg-0ad7b8699e7476134"]
     key_name ="rohi12"
     tags ={
