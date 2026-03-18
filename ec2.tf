1fa5cd766fb32e2a8b32e71211c028b56ed16cea
chaudharirohini20 <chaudharirohini012@gmail.com>
2026-03-18 19:51:59 +0530

    my commit

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,7 @@ provider "aws" {
      region = " eu-west-3"
      }
  
- resource "aws_instamce" "my-instance" {
+ resource "aws_instance" "my-instance" {
     ami = "ami-007dcf089b8078f1a"
     instance_type ="t3.micro"
     vpc_security_group_ids =["sg-0ad7b8699e7476134 "]
