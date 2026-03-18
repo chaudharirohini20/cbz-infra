60f9da8db8fbafb01767a945db8727a5a0949ca5
chaudharirohini20 <chaudharirohini012@gmail.com>
2026-03-18 20:09:47 +0530

    last fix

--- ec2.tf
+++ ec2.tf
@@ -5,7 +5,7 @@ provider "aws" {
  resource "aws_instance" "my-instance" {
     ami = "ami-007dcf089b8078f1a"
     instance_type ="t3.micro"
-    vpc_security_group_ids =["sg-0ad7b8699e7476134 "]
+    vpc_security_group_ids =["sg-0ad7b8699e7476134"]
     key_name ="rohi12"
     tags ={
         name ="my-instance"
